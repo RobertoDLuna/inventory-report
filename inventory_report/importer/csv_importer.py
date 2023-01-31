28c63747605d7bb0bbd7ca116217e8da1b1d4664
Roberto Luna <robertocgw@gmail.com>
2023-01-31 18:36:09 -0300

    refactor: create csvImporter

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -1,1 +1,13 @@
-# Começando
+from inventory_report.importer.importer import Importer
+import csv
+from typing import List, Dict
+
+
+class CsvImporter(Importer):
+    def import_data(path: str) -> List[Dict]:
+        with open(path, encoding="utf-8") as file:
+            if "csv" not in path:
+                raise ValueError("Arquivo inválido")
+            else:
+                data = csv.DictReader(file, delimiter=",", quotechar='"')
+            return list(data)
